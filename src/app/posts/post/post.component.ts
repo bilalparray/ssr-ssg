d8d219231c3f232d07180660f6cd1eb593268350
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-03-14 16:56:36 +0530

    api call

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { log } from 'console';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
-export class PostComponent {
-
+export class PostComponent implements OnInit {
+  constructor(private router: ActivatedRoute) {}
+  url: any;
+  ngOnInit() {
+    this.url = this.router.snapshot.paramMap.get('id');
+    this.getData();
+  }
+  data: any;
+  async getData() {
+    const resp = await fetch(`https://dummyjson.com/products/${this.url}`);
+    this.data = await resp.json();
+    console.log(this.data);
+  }
 }
